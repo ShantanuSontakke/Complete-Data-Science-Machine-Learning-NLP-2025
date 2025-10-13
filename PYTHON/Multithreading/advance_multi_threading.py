bb7d14241c0377199f7d53408a82f34dd6741baf
ShantanuSontakke <shantanusontakke2003@gmail.com>
2025-10-13 23:12:16 +0530

    first commit

--- PYTHON/Multithreading/advance_multi_threading.py
+++ PYTHON/Multithreading/advance_multi_threading.py
@@ -1,4 +1,18 @@
 
-### multithreading with Thread Pool Executior
+# ### multithreading with Thread Pool Executior
 
-form concurrent.futures
+# from concurrent.futures import ThreadPoolExecutor
+# import time
+
+# def print_number(number):
+#     time.sleep(1)
+#     return f"Number : {number}"
+
+# numbers=[1,2,3,4,5,6,7,8   ]
+
+# with ThreadPoolExecutor(max_workers=3) as executor:
+#     results=executor.map(print_number,numbers )
+
+# for result in results:
+#     print(result)
+
